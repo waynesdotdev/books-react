7f423ef9478832588c435988907c4deb863b05dd
waynesdotdev <fox.wayne1@gmail.com>
2024-04-12 08:51:38 +0100

    Learning Context

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,11 +1,19 @@
 import BookShow from "./BookShow.jsx";
+import {useContext} from "react";
+import BooksContext, {Provider} from "../context/books.jsx";
 
 export default function BookList({books, onDelete, onEdit}) {
+    const {count, incrementCount} = useContext(BooksContext)
+
     const renderedBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit}/>
     })
 
     return (
-        <div>{renderedBooks}</div>
+        <div className='book-list'>
+            {count}
+            <button onClick={incrementCount}>Click to Increment</button>
+            {renderedBooks}
+        </div>
     );
 }
